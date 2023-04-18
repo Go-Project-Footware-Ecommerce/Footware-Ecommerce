e9973244643b76b15dc575e1a8dbc4c9877d11c8
Pallavi Kapila <pallavi@infinite.com>
2023-04-18 14:40:04 +0530

    latest committ

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"log"
-	"net/http"
-	"text/template"
 
 	"Footware-Ecommerce/routes"
 
 	"github.com/gin-gonic/gin"
 )
 
+/*
 // variable for template
 var tmpl *template.Template
 
@@ -24,13 +23,14 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	//here i will execute my index file that is my home file
 	tmpl.ExecuteTemplate(w, "index.html", nil)
 }
+*/
 
 func main() {
 	port := "8000"
 
 	router := gin.New()      //To create a new router, use the New() function
 	router.Use(gin.Logger()) //Logger instances a Logger middleware that will write the logs to gin.DefaultWriter.
-	router.LoadHTMLGlob("template/*.html")
+	//router.LoadHTMLGlob("template/*.html")
 	routes.EveyoneShowRoutes(router) //Everyone can see this APIs
 	routes.AdminAuthRoutes(router)   //Required ADMIN token to use this APIs
 	routes.UserAuthRoutes(router)    //Required USER token to use this APIs
